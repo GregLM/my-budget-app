4d0104fdc34ab3d42d185fa04d0d715d054c303c
adm.glourme <adm.glourme@experial-boulogne.local>
2026-02-04 09:42:04 +0100

    Fix : Erreur Settings - Import pb

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo, useEffect } from 'react';
-import { Plus, Home, PieChart, Settings, Wallet, Target, Hourglass } from 'lucide-react';
+import { Plus, Home, PieChart, Settings, Wallet, Target, Hourglass, Trash2 } from 'lucide-react';
 import { v4 as uuidv4 } from 'uuid';
 import { BalanceCard } from '@/app/components/BalanceCard';
 import { CategoryChart } from '@/app/components/CategoryChart';
