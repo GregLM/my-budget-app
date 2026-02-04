c15a49454ca68e51b89a69919031f55c988e318a
adm.glourme <adm.glourme@experial-boulogne.local>
2026-02-04 14:02:28 +0100

    Add : pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,47 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '/my-budget-app/',
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Eco Budget',
+        short_name: 'Budget',
+        description: 'Mon application de budget personnel',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        // IMPORTANT POUR GITHUB PAGES : On précise bien le sous-dossier
+        scope: '/my-budget-app/',
+        start_url: '/my-budget-app/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
